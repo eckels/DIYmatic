8c9b29f4e0f1665e10c61d01a913d9ed699e5f53
Evan Eckels <evan.eckels@yahoo.com>
2016-11-30 22:30:39 -0600

    added filter functionality

--- js/login.js
+++ js/login.js
@@ -4,14 +4,12 @@ function switchTabs(number) {
     $('#registerTab').removeClass("active-tab");
       document.getElementById('login-form').style.display = "block";
       document.getElementById('register-form').style.display = "none";
-    console.log('login');
   }
   if (number === 2) {
     $('#registerTab').addClass("active-tab");
     $('#loginTab').removeClass("active-tab");
       document.getElementById('register-form').style.display = "block";
       document.getElementById('login-form').style.display = "none";
-    console.log('register');
   }
 }
 
@@ -22,3 +20,28 @@ function displayLogin() {
 function closeLogin() {
   document.getElementById('login-screen').style.display = "none";
 }
+
+$(document).ready(function() {
+    console.log('ready');
+  $('#filter').change(function() {
+      var choice = $(this).val();
+    filterItems();
+    function filterItems() {
+        var selectedHashtag = choice;
+        var allImages = document.getElementsByClassName("guide");
+        var selectedImages = document.getElementsByClassName(choice);
+        var allImageIndex;
+        var selectedImageIndex;
+      for (allImageIndex = 0; allImageIndex < allImages.length; allImageIndex++) {
+        allImages[allImageIndex].style.display = "none";
+      }
+      for (selectedImageIndex = 0; selectedImageIndex < selectedImages.length; selectedImageIndex++) {
+        selectedImages[selectedImageIndex].style.display = "inline-block";
+      }
+    }
+    if (choice === "all") {
+      choice = "guide";
+      filterItems();
+    }
+  });
+});
