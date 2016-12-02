c6be0f3fae88518adf74d7f1442f38cb17db975e
Evan Eckels <evan.eckels@yahoo.com>
2016-12-01 23:00:06 -0600

    last upload for night

---
+++ js/create.js
@@ -0,0 +1,8 @@
+/* var matCounter = 2;
+
+$('#materialbutton').click(function() {
+    $('#materialbutton').before('<input type="text" class="text-small" id="material' + matCounter + '"><br>');
+    matCounter++;
+    console.log('werks');
+    console.log(matCounter);
+}); */
